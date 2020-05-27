314f241d530df245448e6514bea532a0a6aabfcf
emmcapor <emmcapor@EMMCAPORW.italy.itroot.adnet>
2020-05-27 09:55:51 +0200

    Modificata la classe StampaController per gestire il codice mancante per i reclami di concessione

--- siesWeb/src/siap/sico/stampa/controller/StampaController.java
+++ siesWeb/src/siap/sico/stampa/controller/StampaController.java
@@ -1114,6 +1114,7 @@ public class StampaController extends SIAPStampaController implements IStampa {
 
 			// DL 92/2014 Rimedi Risarcitori/ Reclamo Rimedi Risarcitori
 			if ("5491".equals(aEveModel.getEvento().getCodMotivo())
+					|| "9154".equals(aEveModel.getEvento().getCodMotivo()) // EC: gestione Ticket#20200508014 (aggiungo il codice mancante 9154) 
 					|| "5492".equals(aEveModel.getEvento().getCodMotivo())
 					|| "5493".equals(aEveModel.getEvento().getCodMotivo())
 					|| "5494".equals(aEveModel.getEvento().getCodMotivo())
