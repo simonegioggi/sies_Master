2f3aec2ebc18ebe3a47f0b8ad89882cfd8bbf255
d.fiorletta <d.fiorletta@DESKTOP-22EAGMC.intersistemi.it>
2020-07-21 13:49:08 +0200

    Aggiunto controllo per gestire assenza Pena Principale in presenza di richieste di Revoca Benefici

--- siesWeb/src/siap/siep/istruttoriacumulo/controller/StampaCumuloController.java
+++ siesWeb/src/siap/siep/istruttoriacumulo/controller/StampaCumuloController.java
@@ -509,9 +509,25 @@ public class StampaCumuloController extends SiapController implements IStampaCum
 								// in caso esista, legata alla PENA COMPLESSIVA
 								// Sanzione Sostitutiva Cumulo
 								SanzioneSostitutivaCumuloModel lSSCumMod1 = new SanzioneSostitutivaCumuloModel();
+
+								//==========================================================================================
+								// Ticket#20200715012 - la pena complessiva potrebbe non essere presente.
+								BigDecimal idPenaCompCumulo = null;
+								if (lPenaCum!=null) 
+									idPenaCompCumulo = lPenaCum.getIdPenaComplessivaCum();
+									
 								lSSCumSqlDao.ricercaSanzioneSostitutivaByIdPenaComplessivaCumTitoloCum(
-										lPenaCum.getIdPenaComplessivaCum(),
+										idPenaCompCumulo,
 										lRichTitCumMod.getTitIdTitoloCumulato());
+								
+//								lSSCumSqlDao.ricercaSanzioneSostitutivaByIdPenaComplessivaCumTitoloCum(
+//										lPenaCum.getIdPenaComplessivaCum(),
+//										lRichTitCumMod.getTitIdTitoloCumulato());								
+								// END Ticket#20200715012
+								//==========================================================================================
+								
+								
+								
 								lSSCumSqlDao.start();
 								while (lSSCumSqlDao.next()) {
 
@@ -1813,11 +1829,26 @@ public class StampaCumuloController extends SiapController implements IStampaCum
 							// Ricerca Sanzione Sost Cum Dao");
 							// ======= >>>>>>>>>>>>>>>>> MEV_70 - Aggiungo sempre la SanzioneSostitutiva, in
 							// caso esista, legata alla PENA COMPLESSIVA
-							// Sanzione Sostitutiva Cumulo
+							// Sanzione Sostitutiva Cumulo							
 							SanzioneSostitutivaCumuloModel lSSCumMod1 = new SanzioneSostitutivaCumuloModel();
+							
+							//==========================================================================================
+							// Ticket#20200715012 - la pena complessiva potrebbe non essere presente.
+							BigDecimal idPenaCompCumulo = null;
+							if (lPenaCum!=null) 
+								idPenaCompCumulo = lPenaCum.getIdPenaComplessivaCum();
+								
 							lSSCumSqlDao.ricercaSanzioneSostitutivaByIdPenaComplessivaCumTitoloCum(
-									lPenaCum.getIdPenaComplessivaCum(),
+									idPenaCompCumulo,
 									lRichTitCumMod.getTitIdTitoloCumulato());
+							
+//							lSSCumSqlDao.ricercaSanzioneSostitutivaByIdPenaComplessivaCumTitoloCum(
+//									lPenaCum.getIdPenaComplessivaCum(),
+//									lRichTitCumMod.getTitIdTitoloCumulato());	
+							
+							// END Ticket#20200715012
+							//==========================================================================================
+							
 							lSSCumSqlDao.start();
 							while (lSSCumSqlDao.next()) {
 
