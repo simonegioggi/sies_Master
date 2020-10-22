8d458cb66b2b8e1e02a15f23edf8307dee45c8f5
simone gioggi <simone.gioggi@eng.it>
2020-10-22 17:23:34 +0200

    Committed on 20201022 by SG

--- siesEsecuzione/src/main/java/it/mig/sies/util/SiesDAO.java
+++ siesEsecuzione/src/main/java/it/mig/sies/util/SiesDAO.java
@@ -221,7 +221,7 @@ public class SiesDAO {
 		SqlSession session = null;
 		try {
 			session = getSession();
-			Map<String, Long> parameters = new HashMap<>(2);
+			Map<String, Long> parameters = new HashMap<String, Long>(2);
 			parameters.put("sies", new Long(chiaviProvvedimentoGiudiziario.getSies().longValue()));
 			parameters.put("nsc", new Long(chiaviProvvedimentoGiudiziario.getNsc().longValue()));
 			int i = session.update("it.mig.sies.model.TitoloGiudiziario.update", parameters);
@@ -476,7 +476,7 @@ public class SiesDAO {
 		SqlSession session = null;
 		try {
 			session = getSession();
-			Map<String, Long> parameters = new HashMap<>(2);
+			Map<String, Long> parameters = new HashMap<String, Long>(2);
 			parameters.put("sies", new Long(chiaviProvvedimentoGiudiziario.getSies().longValue()));
 			int i = session.update("it.mig.sies.model.TitoloGiudiziario.deleteLogica", parameters);
 			logger.info("Update chiavi Titolo Principale: NSC[" + parameters.get("nsc") + "] SIES["
@@ -720,7 +720,7 @@ public class SiesDAO {
 		String codiceOggetto = codiceUnivoco.substring(0, 4);
 		String codiceMotivo = codiceUnivoco.substring(4, 8);
 		String codiceEsito = codiceUnivoco.substring(8, 12);
-		Map<String, String> params = new HashMap<>();
+		Map<String, String> params = new HashMap<String, String>();
 		params.put("codiceOggetto", codiceOggetto);
 		params.put("codiceMotivo", codiceMotivo);
 		params.put("codiceEsito", codiceEsito);
@@ -920,7 +920,7 @@ public class SiesDAO {
 		try {
 			session = getSession();
 
-			Map<String, Long> param = new HashMap<>(2);
+			Map<String, Long> param = new HashMap<String, Long>(2);
 			if (isForCumulo) {
 				param.put("idFascicoloSiep", new Long(idFascicoloSiep));
 				param.put("idEvento", new Long(idEvento));
@@ -1154,7 +1154,7 @@ public class SiesDAO {
 					datiPubblicoMinisteroList.add(0, dpm);
 				}
 			} else if (isAvvenutaEsecuzionePena) {
-				Map<String, Long> parameters = new HashMap<>(2);
+				Map<String, Long> parameters = new HashMap<String, Long>(2);
 				parameters.put("idFascicoloSiep", new Long(idFascicoloSiep));
 				parameters.put("idEvento", new Long(idEvento));
 				datiPubblicoMinisteroList = session
@@ -1235,7 +1235,7 @@ public class SiesDAO {
 		SqlSession session = null;
 		try {
 			session = getSession();
-			Map<String, Long> parameters = new HashMap<>(2);
+			Map<String, Long> parameters = new HashMap<String, Long>(2);
 			parameters.put("sies", new Long(chiaviProvvedimentoGiudiziario.getSies().longValue()));
 			int i = session.update("it.mig.sies.model.TitoloGiudiziario.deleteLogica", parameters);
 			logger.info("Update chiavi Titolo Principale: NSC[" + parameters.get("nsc") + "] SIES["
@@ -1311,7 +1311,7 @@ public class SiesDAO {
 		SqlSession session = null;
 		try {
 			session = getSession();
-			Map<String, Long> parameters = new HashMap<>(2);
+			Map<String, Long> parameters = new HashMap<String, Long>(2);
 			parameters.put("sies", new Long(chiaviProvvedimentoGiudiziario.getSies().longValue()));
 			parameters.put("nsc", new Long(chiaviProvvedimentoGiudiziario.getNsc().longValue()));
 			int i = session.update("it.mig.sies.model.TitoloGiudiziario.update", parameters);
