76db41b81c50c5138437eabc73d36a0921817b82
d.fiorletta <d.fiorletta@DESKTOP-22EAGMC.intersistemi.it>
2020-12-11 18:33:14 +0100

    Corretta gestione Reclusione negativa

--- siesWeb/src/siap/siep/modulocumulo/util/CalcoloPenaCumuloModel.java
+++ siesWeb/src/siap/siep/modulocumulo/util/CalcoloPenaCumuloModel.java
@@ -727,6 +727,10 @@ public class CalcoloPenaCumuloModel extends GenericModel {
     siesLogger.debug("lCalReclusioneTotMod = "+lCalReclusioneTotMod);
     siesLogger.debug("lCalArrestiTotMod = "+lCalArrestiTotMod);
     
+	/* INIZIO ticket#20201204019 in caso di Reclusione negativa, la sposto sull'arresto SOLO al termine 
+	 * del computo di tutte le quantità in quanto potrebbe ritornare positivo a seguito di revoche di benefici.
+	 * Se spostato ora sull'arresto non può più tornare indietro.
+	 * 
 		if (!lCalUtil.isPositiveTime(lCalReclusioneTotMod)) {
 			siesLogger.debug("Attenzione Quantum di Reclusione Negativi: " + lCalReclusioneTotMod);
 
@@ -747,7 +751,10 @@ public class CalcoloPenaCumuloModel extends GenericModel {
 			lCalReclusioneTotMod.setNumGiorni(0);
 			
 		}
-		siesLogger.debug("Totali Parziali Aggiornati ==================");
+		FINE ticket#20201204019
+		*/
+
+    siesLogger.debug("Totali Parziali Aggiornati ==================");
     siesLogger.debug("lCalReclusioneTotMod = "+lCalReclusioneTotMod);
     siesLogger.debug("lCalArrestiTotMod = "+lCalArrestiTotMod);
 
