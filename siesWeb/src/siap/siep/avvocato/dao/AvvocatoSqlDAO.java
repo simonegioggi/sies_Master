bbe06fdb713332dbb2de0091fb3a4c458ce30c83
sgioggi <sgioggi@eng.it>
2021-06-23 11:45:05 +0200

    Committed on 20210623 by SG x MEV_21

--- siesWeb/src/siap/siep/avvocato/dao/AvvocatoSqlDAO.java
+++ siesWeb/src/siap/siep/avvocato/dao/AvvocatoSqlDAO.java
@@ -249,7 +249,7 @@ public class AvvocatoSqlDAO extends SIAPSqlDAO {
 		// MEV_21: aggiungo condizione
 		// lStatement += " (select id_avvocato_standard from avvocato where flag_visualizza = 1 and
 		// FLAG_CANCELLATO ='N'";
-		lStatement += " (select id_avvocato_standard from avvocato where flag_visualizza = 1 and FLAG_CANCELLATO ='N' and FLAG_REGINDE = 'S'";
+		lStatement += " (select id_avvocato_standard from avvocato where flag_visualizza = 1 and FLAG_CANCELLATO ='N' and FLAG_REGINDE = 'SI'";
 		lStatement += " and cod_ufficio_appartenenza = '"
 				+ StringUtils.convertSqlString(aModel.getCodUffAppartenenza().toUpperCase());
 		lStatement += "') and " + lSql2;
