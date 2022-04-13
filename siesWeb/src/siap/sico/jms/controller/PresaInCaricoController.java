351301efaa2bddbd55fb9908323b10b128842d77
sgioggi <sgioggi@eng.it>
2022-04-13 17:09:35 +0200

    Committed on 20220413 for new release_12.4.19.0 by SG

--- siesWeb/src/siap/sico/jms/controller/PresaInCaricoController.java
+++ siesWeb/src/siap/sico/jms/controller/PresaInCaricoController.java
@@ -3291,11 +3291,11 @@ public class PresaInCaricoController extends SiapController implements IPresaInC
 
 				} // endif Natura
 
-				// Ticket#20220331017 - aggiunto controllo sulla presenza dell'ID_FASCICOLO_SIEP a cui 
+				// Ticket#20220331017 - aggiunto controllo sulla presenza dell'ID_FASCICOLO_SIEP a cui
 				// collegare lo STATO_PROCEDIMENTO. Non sempre viene passato.
 				// if (lStatoProcedimento != null) {
-				if (lStatoProcedimento != null && lEve.getFasSieIdFascicoloSiep()!=null) {
-			    // Ticket#20220331017 - FINE	
+				if (lStatoProcedimento != null && lEve != null && lEve.getFasSieIdFascicoloSiep() != null) {
+					// Ticket#20220331017 - FINE
 					lStatoDao = new StatoProcedimentoDAO(aConn);
 					// - Cancella eventuali record prima di inserire un nuovo STATO_PROCEDIMENTO
 					lStatoDao.setCondizioneByIdFascicolo(lEve.getFasSieIdFascicoloSiep());
