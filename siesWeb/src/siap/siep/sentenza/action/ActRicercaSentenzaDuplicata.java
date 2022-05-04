8887b888e357efa81cab15bf1cf1ebfda6ab48c2
d.fiorletta <d.fiorletta@DESKTOP-22EAGMC.intersistemi.it>
2022-05-04 11:07:57 +0200

    Ticket#202204010111
    Modificato ricerca sentenza per restituire un vettore di
    SentenzaFascicoliModel

--- siesWeb/src/siap/siep/sentenza/action/ActRicercaSentenzaDuplicata.java
+++ siesWeb/src/siap/siep/sentenza/action/ActRicercaSentenzaDuplicata.java
@@ -1,5 +1,7 @@
 package siap.siep.sentenza.action;
 
+import java.math.BigDecimal;
+
 /**
  *
  * <p>Title: </p>
@@ -16,6 +18,7 @@ import f3b.web.IWebConstants;
 import siap.sico.utente.model.UtenteModel;
 import siap.sico.web.ActionSiap;
 import siap.siep.sentenza.controller.ISentenza;
+import siap.siep.sentenza.model.SentenzaFascicoliModel;
 import siap.siep.sentenza.model.SentenzaModel;
 import siap.siep.util.SIEPLookupRemote;
 import siap.web.ISIAPCostantiWeb;
@@ -48,15 +51,26 @@ public class ActRicercaSentenzaDuplicata extends ActionSiap implements ICostanti
 		lSmod.setCodUfficioInserimento(lUtenteMod.getUfficioUtente().getCodUfficio());
 
 		ISentenza lSCtrl = SIEPLookupRemote.getSentenzaRemote();
-		Vector lSentenze = lSCtrl.ExRicercaSentenzaDuplicata(lSmod);
-
+		// Ticket#202204010111 - si sostituisce la funzione di ricerca
+	    // Vector lSentenze = lSCtrl.ExRicercaSentenzaDuplicata(lSmod);
+		Vector <SentenzaFascicoliModel> lSentenze = lSCtrl.ExRicercaSentenzaDuplicataFascicoli(lSmod);
+		// Ticket#202204010111 - FINE
+		
 		// imposta la risposta la risposta nella request
 		String lReturnPage = "";
 
 		if (lSentenze.size() == 1) {
+			// Ticket#202204010111 - si recupera la sentenza dal model strutturato
+			/*
 			lReturnPage = IWebConstants.PG_MAIN + "?" + IWebConstants.ACTION_FIELD
 					+ "=siap.siep.sentenza.action.ActLoadInserisciSentenzaDuplicata&" + CAMPO_ID_SENTENZA
-					+ "=" + ((SentenzaModel) lSentenze.get(0)).getIdSentenza().toString();
+					+ "=" + ((SentenzaModel) lSentenze.get(0)).getIdSentenza().toString();*/
+			BigDecimal idSentenza = ((SentenzaFascicoliModel) lSentenze.get(0)).getSentenza().getIdSentenza();
+			
+			lReturnPage = IWebConstants.PG_MAIN + "?" + IWebConstants.ACTION_FIELD
+					+ "=siap.siep.sentenza.action.ActLoadInserisciSentenzaDuplicata&" + CAMPO_ID_SENTENZA
+					+ "=" + idSentenza.toString();
+			// Ticket#202204010111 - FINE
 		} else {
 			setRequestAttribute(IWebConstants.REQUEST_FOR_PAGING, "NO");
 			setRequestAttribute("sentenze", lSentenze);
