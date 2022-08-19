e6ab37b6c513676cfb85e3f5ebe777668e625c36
d.fiorletta <d.fiorletta@DESKTOP-22EAGMC.intersistemi.it>
2022-08-19 16:34:50 +0200

    Change
    Il comune di nascita viene sempre decodificato dal CF se REGINDE

--- siesWeb/src/siap/siep/avvocato/action/ActInserisciAvvocato.java
+++ siesWeb/src/siap/siep/avvocato/action/ActInserisciAvvocato.java
@@ -100,17 +100,21 @@ public class ActInserisciAvvocato extends ActProvvedimentoDifensore implements I
 			if ("manuale".equals(tipoInserimento)) {
 				throw new F3BException(F3BException.USER_MESSAGE, e.getMessage());
 			} else {
-				// 11.05.2022 deve decodificare il comune di nascita dal CF dell'avvocato
-				String codiFiscAvv = getRequestStringParameter(CAMPO_CODICE_FISCALE);
-				comuneNascita = AvvocatoUtil.calcolaComuneNascita(codiFiscAvv);
-				codLuogoNascita = comuneNascita.getCodComune();
-				descCodLuogoNascita = comuneNascita.getDescrizione();
-				codCap = comuneNascita.getCap();
-				codProvincia = comuneNascita.getCodProvincia();
-				// Salvo comunque la descrizione del comune di nascita reginde per tenerne traccia
-				descLuogoNascitaReginde = getRequestStringParameter(CAMPO_COD_LUOGO_NASCITA);
-				// 11.05.2022 FINE
+				// non faccio nulla recupero dopo il comune dal CF
 			}
+		}
+		
+		// In caso di iscrizione reginde o CERTIFICATO SIES il comune di nascita va sempre decodificato 
+		// dal codice fiscale indipendentemente dal Comune riportato in form
+		if (!"manuale".equals(tipoInserimento)) {
+			String codiFiscAvv = getRequestStringParameter(CAMPO_CODICE_FISCALE);
+			comuneNascita = AvvocatoUtil.calcolaComuneNascita(codiFiscAvv);
+			codLuogoNascita = comuneNascita.getCodComune();
+			descCodLuogoNascita = comuneNascita.getDescrizione();
+			codCap = comuneNascita.getCap();
+			codProvincia = comuneNascita.getCodProvincia();
+			// Salvo comunque la descrizione del comune di nascita reginde per tenerne traccia
+			descLuogoNascitaReginde = getRequestStringParameter(CAMPO_COD_LUOGO_NASCITA);
 		}
 
 		if (getRequestStringParameter(CAMPO_DESC_COMUNE_NASCITA_REGINDE).length() > 0)
--- siesWeb/src/siap/sige/avvocato/action/ActSostituzioneDifensore.java
+++ siesWeb/src/siap/sige/avvocato/action/ActSostituzioneDifensore.java
@@ -96,19 +96,23 @@ public class ActSostituzioneDifensore extends ActionSiap implements ICostantiAvv
 			if ("manuale".equals(tipoInserimento)) {
 				throw new F3BException(F3BException.USER_MESSAGE, e.getMessage());
 			} else {
-				// deve decodificare il comune di nascita dal CF dell'avvocato
-				String codiFiscAvv = getRequestStringParameter(CAMPO_CODICE_FISCALE);
-				comuneNascita = AvvocatoUtil.calcolaComuneNascita(codiFiscAvv);
-				codLuogoNascita = comuneNascita.getCodComune();
-				descCodLuogoNascita = comuneNascita.getDescrizione();
-				codCap = comuneNascita.getCap();
-				codProvincia = comuneNascita.getCodProvincia();
-				// Salvo comunque la descrizione del comune di nascita reginde per tenerne traccia
-				descLuogoNascitaReginde = getRequestStringParameter(CAMPO_COD_LUOGO_NASCITA);
-				// FINE
+			// non faccio nulla recupero dopo il comune dal CF
 			}			
 		}
 
+		// In caso di iscrizione reginde o CERTIFICATO SIES il comune di nascita va sempre decodificato 
+		// dal codice fiscale indipendentemente dal Comune riportato in form
+		if (!"manuale".equals(tipoInserimento)) {
+			String codiFiscAvv = getRequestStringParameter(CAMPO_CODICE_FISCALE);
+			comuneNascita = AvvocatoUtil.calcolaComuneNascita(codiFiscAvv);
+			codLuogoNascita = comuneNascita.getCodComune();
+			descCodLuogoNascita = comuneNascita.getDescrizione();
+			codCap = comuneNascita.getCap();
+			codProvincia = comuneNascita.getCodProvincia();
+			// Salvo comunque la descrizione del comune di nascita reginde per tenerne traccia
+			descLuogoNascitaReginde = getRequestStringParameter(CAMPO_COD_LUOGO_NASCITA);
+		}
+		
 		if (getRequestStringParameter(CAMPO_DESC_COMUNE_NASCITA_REGINDE).length() > 0)
 			descLuogoNascitaReginde = getRequestStringParameter(CAMPO_DESC_COMUNE_NASCITA_REGINDE);
 
