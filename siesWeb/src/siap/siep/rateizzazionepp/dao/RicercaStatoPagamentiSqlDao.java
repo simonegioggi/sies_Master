754685bccbec12d20dfaa3e27f7f86fec4ba56f8
d.fiorletta <d.fiorletta@DESKTOP-22EAGMC.intersistemi.it>
2023-09-12 18:05:46 +0200

    MEV_2023-33
    modificata ricerca stato pagamenti per prendere in considerazione solo
    le rate collegate ad eventi validati

--- siesWeb/src/siap/siep/rateizzazionepp/dao/RicercaStatoPagamentiSqlDao.java
+++ siesWeb/src/siap/siep/rateizzazionepp/dao/RicercaStatoPagamentiSqlDao.java
@@ -78,39 +78,58 @@ public class RicercaStatoPagamentiSqlDao extends SIAPSqlDAO {
 
 		String lSql = "";
 
-		lSql += " SELECT FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
-		lSql += "      , FASCICOLO_SIEP.CHIAVE_ANNO, FASCICOLO_SIEP.CHIAVE_PROGR, FASCICOLO_SIEP.DATA_ISCRIZIONE ";
-		lSql += "      , SOGGETTO.NOME, SOGGETTO.COGNOME ";
-		lSql += "      , rateizzazione.TIPO_RATEIZZAZIONE ";
-		lSql += "      , rateizzazione.importoDaPagare";
-		lSql += "      , statoPagamenti.importoPagato";
-		lSql += "      , ultimaScadenza.dataUltimaScadenza ";
-		lSql += "   FROM FASCICOLO_SIEP, SOGGETTO ";
-		lSql += "      , (SELECT SUM(IMPORTO_RATA*NUMERO_RATE) importoDaPagare, FAS_SIE_ID_FASCICOLO_SIEP, TIPO_RATEIZZAZIONE ";
-		lSql += "           FROM RATEIZZAZIONE_PP ";
-		lSql += "       GROUP BY FAS_SIE_ID_FASCICOLO_SIEP, TIPO_RATEIZZAZIONE) rateizzazione ";
-		lSql += "      , (SELECT NVL(SUM(IMPORTO_PAGATO),0) importoPagato, FAS_SIE_ID_FASCICOLO_SIEP ";
-		lSql += "           FROM BOLLETTINO_PAGOPA ";
-		lSql += "       GROUP BY FAS_SIE_ID_FASCICOLO_SIEP) statoPagamenti ";
-		lSql += "      , (SELECT MAX(DATA_SCADENZA) dataUltimaScadenza, FAS_SIE_ID_FASCICOLO_SIEP ";
-		lSql += "           FROM BOLLETTINO_PAGOPA ";
-		lSql += "       GROUP BY FAS_SIE_ID_FASCICOLO_SIEP ) ultimaScadenza ";
-		// In caso di ricerca di bollettini non pagati sono interessato solo ai bollettini scaduti al momento della ricerca
-	    if (   aTipoRicera.equals(ICostantiSanzioneSostitutiva.CAMPO_TIPO_RICERCA_RETEIZZATO_NON_PAGATO)
-	    	|| aTipoRicera.equals(ICostantiSanzioneSostitutiva.CAMPO_TIPO_RICERCA_UNICA_RATA_NON_PAGATO)	
-   		   ) {
-			lSql += "        , (SELECT DISTINCT FAS_SIE_ID_FASCICOLO_SIEP ";
-			lSql += "                FROM BOLLETTINO_PAGOPA ";
-			lSql += "               WHERE (   NVL(BOLLETTINO_PAGOPA.IMPORTO_PAGATO,0) = 0 ";
-			lSql += "                      OR BOLLETTINO_PAGOPA.IMPORTO_PAGATO < BOLLETTINO_PAGOPA.IMPORTO_RATA) "; // non interamente pagato
-			lSql += "                 AND BOLLETTINO_PAGOPA.DATA_SCADENZA < sysdate ) scaduti ";	
-	    }
-		
-		lSql += "  WHERE 1=1 ";
-		lSql += "    AND FASCICOLO_SIEP.SOG_ID_SOGGETTO = SOGGETTO.ID_SOGGETTO ";
-		lSql += "    AND rateizzazione.FAS_SIE_ID_FASCICOLO_SIEP = FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
-		lSql += "    AND statoPagamenti.FAS_SIE_ID_FASCICOLO_SIEP = FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
-		lSql += "    AND ultimaScadenza.FAS_SIE_ID_FASCICOLO_SIEP = FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
+    lSql += " SELECT FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
+    lSql += "      , FASCICOLO_SIEP.CHIAVE_ANNO, FASCICOLO_SIEP.CHIAVE_PROGR, FASCICOLO_SIEP.DATA_ISCRIZIONE ";
+    lSql += "      , SOGGETTO.NOME, SOGGETTO.COGNOME ";
+    lSql += "      , rateizzazione.TIPO_RATEIZZAZIONE ";
+    lSql += "      , rateizzazione.importoDaPagare";
+    lSql += "      , statoPagamenti.importoPagato";
+    lSql += "      , ultimaScadenza.dataUltimaScadenza ";
+    lSql += "   FROM FASCICOLO_SIEP, SOGGETTO ";
+    lSql += "      , (SELECT SUM(IMPORTO_RATA*NUMERO_RATE) importoDaPagare, RATEIZZAZIONE_PP.FAS_SIE_ID_FASCICOLO_SIEP, TIPO_RATEIZZAZIONE ";
+    // 12.09.2023 si filtrano solo i bollettini collegati ad eventi validati
+    lSql += "           FROM RATEIZZAZIONE_PP, EVENTO ";
+    lSql += "          WHERE RATEIZZAZIONE_PP.EVE_ID_EVENTO = EVENTO.ID_EVENTO ";
+    lSql += "            AND EVENTO.FLAG_DOCUMENTO_REGISTRATO = 'S' ";
+    // Fine
+    lSql += "       GROUP BY RATEIZZAZIONE_PP.FAS_SIE_ID_FASCICOLO_SIEP, TIPO_RATEIZZAZIONE) rateizzazione ";
+    lSql += "      , (SELECT NVL(SUM(IMPORTO_PAGATO),0) importoPagato, BOLLETTINO_PAGOPA.FAS_SIE_ID_FASCICOLO_SIEP ";
+    // 12.09.2023 si filtrano solo i bollettini collegati ad eventi validati
+    lSql += "           FROM BOLLETTINO_PAGOPA, RATEIZZAZIONE_PP, EVENTO ";
+    lSql += "          WHERE BOLLETTINO_PAGOPA.RAT_ID_RATEIZZAZIONE_PP = RATEIZZAZIONE_PP.ID_RATEIZZAZIONE_PP ";
+    lSql += "            AND RATEIZZAZIONE_PP.EVE_ID_EVENTO = EVENTO.ID_EVENTO ";
+    lSql += "            AND EVENTO.FLAG_DOCUMENTO_REGISTRATO = 'S' ";
+    // Fine
+    lSql += "       GROUP BY BOLLETTINO_PAGOPA.FAS_SIE_ID_FASCICOLO_SIEP) statoPagamenti ";
+    lSql += "      , (SELECT MAX(DATA_SCADENZA) dataUltimaScadenza, BOLLETTINO_PAGOPA.FAS_SIE_ID_FASCICOLO_SIEP ";
+    // 12.09.2023 si filtrano solo i bollettini collegati ad eventi validati
+    lSql += "           FROM BOLLETTINO_PAGOPA, RATEIZZAZIONE_PP, EVENTO ";
+    lSql += "          WHERE BOLLETTINO_PAGOPA.RAT_ID_RATEIZZAZIONE_PP = RATEIZZAZIONE_PP.ID_RATEIZZAZIONE_PP ";
+    lSql += "            AND RATEIZZAZIONE_PP.EVE_ID_EVENTO = EVENTO.ID_EVENTO ";
+    lSql += "            AND EVENTO.FLAG_DOCUMENTO_REGISTRATO = 'S' ";
+    // Fine
+    lSql += "       GROUP BY BOLLETTINO_PAGOPA.FAS_SIE_ID_FASCICOLO_SIEP ) ultimaScadenza ";
+    // In caso di ricerca di bollettini non pagati sono interessato solo ai bollettini scaduti al momento della ricerca
+    if (   aTipoRicera.equals(ICostantiSanzioneSostitutiva.CAMPO_TIPO_RICERCA_RETEIZZATO_NON_PAGATO)
+        || aTipoRicera.equals(ICostantiSanzioneSostitutiva.CAMPO_TIPO_RICERCA_UNICA_RATA_NON_PAGATO)  
+       ) {
+      lSql += "        , (SELECT DISTINCT BOLLETTINO_PAGOPA.FAS_SIE_ID_FASCICOLO_SIEP ";
+      // 12.09.2023 si filtrano solo i bollettini collegati ad eventi validati
+      lSql += "             FROM BOLLETTINO_PAGOPA, RATEIZZAZIONE_PP, EVENTO ";
+      lSql += "            WHERE BOLLETTINO_PAGOPA.RAT_ID_RATEIZZAZIONE_PP = RATEIZZAZIONE_PP.ID_RATEIZZAZIONE_PP ";
+      lSql += "              AND RATEIZZAZIONE_PP.EVE_ID_EVENTO = EVENTO.ID_EVENTO ";
+      lSql += "              AND EVENTO.FLAG_DOCUMENTO_REGISTRATO = 'S' "; 
+      // Fine
+      lSql += "              AND (   NVL(BOLLETTINO_PAGOPA.IMPORTO_PAGATO,0) = 0 ";
+      lSql += "                      OR BOLLETTINO_PAGOPA.IMPORTO_PAGATO < BOLLETTINO_PAGOPA.IMPORTO_RATA) "; // non interamente pagato
+      lSql += "              AND BOLLETTINO_PAGOPA.DATA_SCADENZA < sysdate ) scaduti "; 
+    }
+    
+    lSql += "  WHERE 1=1 ";
+    lSql += "    AND FASCICOLO_SIEP.SOG_ID_SOGGETTO = SOGGETTO.ID_SOGGETTO ";
+    lSql += "    AND rateizzazione.FAS_SIE_ID_FASCICOLO_SIEP = FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
+    lSql += "    AND statoPagamenti.FAS_SIE_ID_FASCICOLO_SIEP = FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
+    lSql += "    AND ultimaScadenza.FAS_SIE_ID_FASCICOLO_SIEP = FASCICOLO_SIEP.ID_FASCICOLO_SIEP ";
 
 		// Ulteriori condizioni
 		lSql += "    AND FASCICOLO_SIEP.CHIAVE_UFFICIO = '" + aFasMod.getChiaveUfficio() + "' ";
