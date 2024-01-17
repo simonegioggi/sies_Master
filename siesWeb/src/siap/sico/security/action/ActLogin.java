474ed344faafdad166941bbc3e62c134a36e05fd
difiorlett <difiorlett@161.27.17.184>
2024-01-17 12:11:55 +0100

    MEV_2023-33
    Correzione visibilità icona "errore pagopa" e correzione gestione
    sessione sulla ActLogin

--- siesWeb/src/siap/sico/security/action/ActLogin.java
+++ siesWeb/src/siap/sico/security/action/ActLogin.java
@@ -27,7 +27,18 @@ public class ActLogin extends ActionSiap implements ICostantiSecurity {
 
 	@SuppressWarnings("rawtypes")
 	public String processRequest() throws F3BException {
-		// Rimuove dalla sessione gli oggetti indicati eventualmente presenti
+
+	  // 2024.01.17 La sessione va invalidata e ricreata altrimenti 
+	  // se l'utente si limita a chiudere la pagina principale e a cambiare
+	  // utenza resta attiva la vecchia sessione che contiene i dati del precedente
+	  // utente se non rimossi
+	  if (getRequest().getSession(false)!=null) {
+	    invalidateSession();
+	    // si crea e registra la nuova sessione
+	    super.setReqSes (getRequest(), getRequest().getSession());
+	  }
+	  
+		// Rimuove dalla sessione gli oggetti indicati eventualmente presenti	  
 		removeSessionAttribute("soggetto");
 		removeSessionAttribute("sentenza");
 		removeSessionAttribute("fascicolo");
@@ -39,7 +50,7 @@ public class ActLogin extends ActionSiap implements ICostantiSecurity {
 		removeSessionAttribute(FUN_RADICE_MENU_ORZ);
 		removeSessionAttribute(FUN_RADICE_MENU_SR);
 		removeSessionAttribute(FUN_ANTENATE);
-
+		
 		// prepara il model dell'utente per la login
 		String lUserId = StringUtils.convertSqlString(getRequestStringParameter(CAMPO_USER_ID));
 		String lPassword = getRequestStringParameter(CAMPO_PASSWORD);
@@ -89,7 +100,14 @@ public class ActLogin extends ActionSiap implements ICostantiSecurity {
 
 		
 		// MEV_2023-33 - Se amministratore di sistema verifico l'ultima esecuzione del batch
-		if (lUtente.getUserProfile().getProfileId().intValue() == 99) {
+		//               Si estende a tutte le utenze SIEP
+		if (   lUtente.getUserProfile().getProfileId().intValue() == 99
+        || lUtente.getUserProfile().getProfileId().intValue() == 90
+        || lUtente.getUserProfile().getProfileId().intValue() == 4
+        || lUtente.getUserProfile().getProfileId().intValue() == 40
+        || lUtente.getUserProfile().getProfileId().intValue() == 50
+       ) 
+		{
 		  try {
   		  IBatchPagopa lCtrlBatch = SIEPLookupRemote.getBatchPagopaPagopaRemote();
   		  BatchPagopaModel lancioBatch = lCtrlBatch.getLastEsecuzioneBatch();
@@ -103,6 +121,10 @@ public class ActLogin extends ActionSiap implements ICostantiSecurity {
 		  } catch (Exception e) {
 		    siesLogger.error("Errore in fase di verifica dell'ultimo run del batch pagopas",e);
 		  }  
+		}
+		else {
+		  //removeSessionAttribute("ErroreBatchPagoPa");
+		  //removeSessionAttribute("BatchPagoPa");
 		}
 	  // MEV_2023-33 - FINE
 		
