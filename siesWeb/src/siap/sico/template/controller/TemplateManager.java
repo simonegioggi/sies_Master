db11390fb72ae9da293fd7c79dbf42413d5c1a0b
sgioggi <simone.gioggi@eng.it>
2024-08-21 14:39:32 +0200

    Realizzazione MEV_2023-35 2024-08-21

--- siesWeb/src/siap/sico/template/controller/TemplateManager.java
+++ siesWeb/src/siap/sico/template/controller/TemplateManager.java
@@ -6,14 +6,14 @@ import java.util.Vector;
 
 import org.apache.log4j.Logger;
 
-import siap.sico.template.model.TemplateModel;
-import siap.sico.util.SICOLookupRemote;
 import f3b.log.LogF3B;
 import f3b.util.F3BException;
+import siap.sico.template.model.TemplateModel;
+import siap.sico.util.SICOLookupRemote;
 
 /**
  * TemplateManager - Classe singleton per il caricamento dei templates da stampare
- * 
+ *
  * @version 1.0
  */
 @SuppressWarnings({ "rawtypes", "unchecked" })
@@ -51,10 +51,11 @@ public class TemplateManager {
 			Iterator lItx = lVector.iterator();
 			while (lItx.hasNext()) {
 				TemplateModel lTemp = new TemplateModel((TemplateModel) lItx.next());
-				// mTemplates.put(lTemp.getIdTemplate(), lTemp.getPathRicerca() + lTemp.getNomeTemplate());
+				mTemplates.put(lTemp.getIdTemplate(), lTemp.getPathRicerca() + lTemp.getNomeTemplate());
 				// FIXME: commentare - vale solo per LOCALHOST
-				String pathRicerca = lTemp.getPathRicerca().replace("/", "\\").replace("\\var\\SIES", "C:");
-				mTemplates.put(lTemp.getIdTemplate(), pathRicerca + lTemp.getNomeTemplate());
+				// String pathRicerca = lTemp.getPathRicerca().replace("/", "\\").replace("\\var\\SIES",
+				// "C:");
+				// mTemplates.put(lTemp.getIdTemplate(), pathRicerca + lTemp.getNomeTemplate());
 				// TODO: commentare - vale solo per LOCALHOST
 			}
 		} catch (F3BException ex) {
