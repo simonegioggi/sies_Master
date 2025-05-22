8e2af652242181b353d30cf046b5965bf71e823c
sgioggi <simone.gioggi@eng.it>
2025-05-22 13:04:00 +0200

    Rilascio_20250627-V_S_12.7.2.0 (Patch Mesi di Marzo, Aprile, Maggio e Giugno 2025)

--- siesWeb/src/siap/sius/documentoallegato/action/ActLoadDocumentoAllegato.java
+++ siesWeb/src/siap/sius/documentoallegato/action/ActLoadDocumentoAllegato.java
@@ -1,35 +1,24 @@
 package siap.sius.documentoallegato.action;
 
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: </p>
- * @author unascribed
- * @version 1.0
- */
-
 import java.io.ByteArrayOutputStream;
 
+import f3b.web.IWebConstants;
 import siap.sico.web.ActionSiap;
 import siap.sius.documentoallegato.controller.IDocumentoAllegato;
 import siap.sius.util.SIUSLookupRemote;
-import f3b.web.IWebConstants;
 
+public class ActLoadDocumentoAllegato extends ActionSiap implements ICostantiDocumentoAllegato {
 
-public class ActLoadDocumentoAllegato extends ActionSiap
-  implements ICostantiDocumentoAllegato
-{
+	public String processRequest() throws Exception {
 
-   public String processRequest() throws Exception
-    {
+		IDocumentoAllegato lCtrl = SIUSLookupRemote.getDocumentoAllegatoRemote();
+		ByteArrayOutputStream lReport = lCtrl
+				.ExGetDocumentoByKey(getRequestBigDecimalParameter(CAMPO_ID_DOCUMENTO_ALLEGATO));
 
-    IDocumentoAllegato lCtrl = SIUSLookupRemote.getDocumentoAllegatoRemote();
-    ByteArrayOutputStream lReport = lCtrl.ExGetDocumentoByKey(getRequestBigDecimalParameter( CAMPO_ID_DOCUMENTO_ALLEGATO));
+		// Prepara la pagina di destinazione
+		setRequestAttribute("report", lReport);
 
-    //Prepara la pagina di destinazione
-    setRequestAttribute("report", lReport);
+		return IWebConstants.PG_DOWNLOAD;
+	}
 
-    return IWebConstants.PG_DOWNLOAD;
-    }
 }
