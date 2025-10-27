c4fd23656a7bcc5ec2560449b05ef4d3637092a7
sgioggi <simone.gioggi@eng.it>
2025-10-27 11:33:52 +0100

    Rilascio_20251024-V_S_12.8.3.0 (Patch Mesi di Settembre e Ottobre 2025)

--- siesWeb/src/siap/sico/soggetto/controller/SoggettoController.java
+++ siesWeb/src/siap/sico/soggetto/controller/SoggettoController.java
@@ -58,18 +58,9 @@ import siap.sius.fascicolo.dao.FascicoloSiusSqlDAO;
 import siap.sius.fascicolo.model.FascicoloSiusModel;
 
 /**
+ * SoggettoController - Classe controller del Soggetto
  *
- * <p>
- * Title: SoggettoController
- * </p>
- * <p>
- * Description: Classe controller del Soggetto
- * </p>
- * <p>
- * </p>
- * <p>
- * Company: Bull Italia S.p.A.
- * </p>
+ * @version 1.0
  */
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class SoggettoController extends SiapController implements ISoggetto {
@@ -904,6 +895,11 @@ public class SoggettoController extends SiapController implements ISoggetto {
 				lStoricoSogg.setDataNascita(lSoggVecchio.getDataNascita());
 				lStoricoSogg.setDataNascitaPresunta(lSoggVecchio.getDataNascitaPresunta());
 
+				// 20251021 [SG] : x trasmissione FC
+				if (aSoggetto != null && lSoggVecchio != null && lSoggVecchio.getKeySoggNsc() != null
+						&& aSoggetto.getKeySoggNsc() == null)
+					aSoggetto.setKeySoggNsc(lSoggVecchio.getKeySoggNsc());
+
 				// MODIFICO SOGGETTO
 				// [FT] - 03/08/2016 - MAC_LOG - Utilizzo la variabile di istanza siesLogger al posto di mLog
 				siesLogger.info("soggetto  ---->" + aSoggetto);
@@ -1227,7 +1223,7 @@ public class SoggettoController extends SiapController implements ISoggetto {
 						modificaNumFasc += aSoggettoVecchio.getCognome() + " " + aSoggettoVecchio.getNome()
 								+ " nato a " + aSoggettoVecchio.getDescrComuneNascita() + " il "
 								+ DateUtils.getDateToString(aSoggettoVecchio.getDataNascita(), "dd-MM-yyyy");
-						;
+
 						modificaDaPassare = modifica + modificaNumFasc + modificaSeguito;
 						aSoggetto.setMessage(modificaDaPassare);
 						lSogDuplicato = aSoggetto;
