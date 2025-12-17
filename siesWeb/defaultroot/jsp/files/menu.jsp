0d0399977703a8ce80773c2e3991e7817a025195
sgioggi <simone.gioggi@eng.it>
2025-12-17 13:26:22 +0100

    20251217: MEV_2025-48: commit 7 files

--- siesWeb/defaultroot/jsp/files/menu.jsp
+++ siesWeb/defaultroot/jsp/files/menu.jsp
@@ -152,15 +152,11 @@ if (lUteMod.getUserProfile().getProfileId().intValue() != 99
     <tr><td><br>&nbsp;</td></tr>
     <tr>
         <td class="menulines" align="left">
-            <a style="" onclick="Javascript:BoldIT('<%=i%>');" 
-                href="/jsp/Main.jsp?<%=IWebConstants.ACTION_FIELD%>=siap.siep.calcolopena.action.ActLoadCalcoloPenaDL92"
-                target="body"
-                >
-                <img align="middle" src="../../images/CalcDL92.png" width="32" height="32" alt="" border="0">
-                &nbsp;&nbsp;&nbsp;&nbsp;Calcolo Pena DL92/2024
+            <a style="" onclick="Javascript:BoldIT('<%=i%>');" href="/jsp/Main.jsp?<%=IWebConstants.ACTION_FIELD%>=siap.siep.calcolopena.action.ActLoadCalcoloPenaDL92" target="body">
+                <img align="middle" src="../../images/CalcDL92.png" width="32" height="32" alt="" border="0">&nbsp;&nbsp;&nbsp;&nbsp;Calcolo Pena DL92/2024
             </a>
         </td>
-    </tr>    
+    </tr>
 <%
 	} else if (codUff.equals("PGCAP") || codUff.equals("PM") || codUff.equals("PMM")) {
 %>
--- siesWeb/src/siap/siep/calcolopena/model/SemestreDL92Model.java
+++ siesWeb/src/siap/siep/calcolopena/model/SemestreDL92Model.java
@@ -6,8 +6,8 @@ import java.util.Date;
 import f3b.model.GenericModel;
 
 /**
- * Model che veicola i risultati dell'elaborazione del conteggio del calcolo delle LA da concedere
- * per semestri secondo quanto previsto dal DL 92/2024 Pena Virtuale
+ * Model che veicola i risultati dell'elaborazione del conteggio del calcolo delle LA da concedere per
+ * semestri secondo quanto previsto dal DL 92/2024 Pena Virtuale
  * 
  * @since MEV_2024-092
  *
@@ -16,7 +16,8 @@ public class SemestreDL92Model extends GenericModel {
 
 	private static final long serialVersionUID = -3682000784946215279L;
 	
-	// SemestriDL92Model: {progressivo, ResiduoAA, ResiduoMM, ResiduoGG, LAApplicata, DataMaturazioneLA, DataScadenzaPena}
+	// SemestriDL92Model: {progressivo, ResiduoAA, ResiduoMM, ResiduoGG, LAApplicata, DataMaturazioneLA,
+	// DataScadenzaPena}
 	private BigDecimal mProgressivo; // Progressivo semestre
 	private BigDecimal mNumSemestriMaturati; // default 1 tranne per il record del presofferto
 	private BigDecimal mResiduoNumAnni;
@@ -31,29 +32,95 @@ public class SemestreDL92Model extends GenericModel {
 	// datao calcolato sono i gg del presofferto che non maturano LA
 	private BigDecimal mGiorniResiduiPresofferto;
 	
+	private String mIsCompreso = "S"; // S/N indica se il semestre va consideratoai fini delle LA
+
   // Metodi Getter
-  public BigDecimal getProgressivo()              { return mProgressivo;  }
-  public BigDecimal getResiduoNumAnni()           { return mResiduoNumAnni; }
-  public BigDecimal getResiduoNumMesi()           { return mResiduoNumMesi; }
-  public BigDecimal getResiduoNumGiorni()         { return mResiduoNumGiorni; }
-  public BigDecimal getLAApplicate()              { return mLAApplicate;  }
-  public Date       getDataMaturazioneLA()        { return mDataMaturazioneLA;  }
-  public Date       getNuovaDataScadenzaPena()    { return mNuovaDataScadenzaPena;  }
-  public BigDecimal getGiorniResiduiPresofferto() { return mGiorniResiduiPresofferto;  }
-  public BigDecimal getNumSemestriMaturati()      { return mNumSemestriMaturati;  }
-  public String     getIsPresofferto()            { return mIsPresofferto;  }
+	public BigDecimal getProgressivo() {
+		return mProgressivo;
+	}
+
+	public BigDecimal getResiduoNumAnni() {
+		return mResiduoNumAnni;
+	}
+
+	public BigDecimal getResiduoNumMesi() {
+		return mResiduoNumMesi;
+	}
+
+	public BigDecimal getResiduoNumGiorni() {
+		return mResiduoNumGiorni;
+	}
+
+	public BigDecimal getLAApplicate() {
+		return mLAApplicate;
+	}
+
+	public Date getDataMaturazioneLA() {
+		return mDataMaturazioneLA;
+	}
+
+	public Date getNuovaDataScadenzaPena() {
+		return mNuovaDataScadenzaPena;
+	}
+
+	public BigDecimal getGiorniResiduiPresofferto() {
+		return mGiorniResiduiPresofferto;
+	}
+
+	public BigDecimal getNumSemestriMaturati() {
+		return mNumSemestriMaturati;
+	}
+
+	public String getIsPresofferto() {
+		return mIsPresofferto;
+	}
   
+	public String getIsCompreso() {
+		return mIsCompreso;
+	}
   
   // Metodi Setter
-  public void setProgressivo              (BigDecimal mProgressivo)     { this.mProgressivo = mProgressivo; }
-  public void setResiduoNumAnni           (BigDecimal mResiduoNumAnni)  { this.mResiduoNumAnni = mResiduoNumAnni; }
-  public void setResiduoNumMesi           (BigDecimal mResiduoNumMesi)  { this.mResiduoNumMesi = mResiduoNumMesi; }
-  public void setResiduoNumGiorni         (BigDecimal mResiduoNumGiorni){ this.mResiduoNumGiorni = mResiduoNumGiorni; }
-  public void setLAApplicate              (BigDecimal mLAApplicate)     { this.mLAApplicate = mLAApplicate; }
-  public void setDataMaturazioneLA        (Date mDataMaturazioneLA)     { this.mDataMaturazioneLA = mDataMaturazioneLA; }
-  public void setNuovaDataScadenzaPena    (Date mNuovaDataScadenzaPena) { this.mNuovaDataScadenzaPena = mNuovaDataScadenzaPena; }
-  public void setGiorniResiduiPresofferto (BigDecimal mGiorniResiduiPresofferto) { this.mGiorniResiduiPresofferto = mGiorniResiduiPresofferto; }
-  public void setNumSemestriMaturati      (BigDecimal mNumSemestriMaturati)  { this.mNumSemestriMaturati = mNumSemestriMaturati; }
-  public void setIsPresofferto            (String mIsPresofferto)            { this.mIsPresofferto = mIsPresofferto; }
+	public void setProgressivo(BigDecimal mProgressivo) {
+		this.mProgressivo = mProgressivo;
+	}
+
+	public void setResiduoNumAnni(BigDecimal mResiduoNumAnni) {
+		this.mResiduoNumAnni = mResiduoNumAnni;
+	}
+
+	public void setResiduoNumMesi(BigDecimal mResiduoNumMesi) {
+		this.mResiduoNumMesi = mResiduoNumMesi;
+	}
+
+	public void setResiduoNumGiorni(BigDecimal mResiduoNumGiorni) {
+		this.mResiduoNumGiorni = mResiduoNumGiorni;
+	}
+
+	public void setLAApplicate(BigDecimal mLAApplicate) {
+		this.mLAApplicate = mLAApplicate;
+	}
+
+	public void setDataMaturazioneLA(Date mDataMaturazioneLA) {
+		this.mDataMaturazioneLA = mDataMaturazioneLA;
+	}
+
+	public void setNuovaDataScadenzaPena(Date mNuovaDataScadenzaPena) {
+		this.mNuovaDataScadenzaPena = mNuovaDataScadenzaPena;
+	}
+
+	public void setGiorniResiduiPresofferto(BigDecimal mGiorniResiduiPresofferto) {
+		this.mGiorniResiduiPresofferto = mGiorniResiduiPresofferto;
+	}
+
+	public void setNumSemestriMaturati(BigDecimal mNumSemestriMaturati) {
+		this.mNumSemestriMaturati = mNumSemestriMaturati;
+	}
+
+	public void setIsPresofferto(String mIsPresofferto) {
+		this.mIsPresofferto = mIsPresofferto;
+	}
   
+	public void setIsCompreso(String mIsCompreso) {
+		this.mIsCompreso = mIsCompreso;
+	}
 }
